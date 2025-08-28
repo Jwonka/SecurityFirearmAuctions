bc7a521358bee98d53d5875c18142500a4b7de7c
Joshua Werlein <jwerlein2@student.cvtc.edu>
2025-08-27 21:04:31 -0500

    Update prices.js

--- js/prices.js
+++ js/prices.js
@@ -1,17 +1,15 @@
 window.pricesByPath = {
   // Handguns
   "images/guns/handguns/9mmGlock.jpg": 529.99,
-  "images/guns/handguns/9mmHandgun.jpg": 399.00,          // generic 9mm handgun
   "images/guns/handguns/HiPoint9mm.jpg": 189.99,
-  "images/guns/handguns/WalterPPK.jpg": 699.00,           // Walther PPK
+  "images/guns/handguns/WaltherPPK.jpg": 699.00, 
   "images/guns/handguns/beretta-handgun-001.jpg": 749.00,
-  "images/guns/handguns/beretta-handgun-002.jpg": 769.00,
+  "images/guns/handguns/springfield-handgun.jpg": 769.00,
   "images/guns/handguns/smith-wesson-handgun-001.jpg": 649.00,
   "images/guns/handguns/taurus-handgun-001.jpg": 349.00,
 
   // Revolvers
   "images/guns/revolvers/38Special.jpg": 499.00,
-  "images/guns/revolvers/38spl-blazer-001.jpg": 519.00,
   "images/guns/revolvers/Smith&Wesson9mmRevolver.jpg": 799.00,
   "images/guns/revolvers/colt-revolver-001.jpg": 1199.00,
 

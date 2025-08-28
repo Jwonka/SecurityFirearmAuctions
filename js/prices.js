f20aad1ee380d851d820143ae9f1ef0dafb9982a
Joshua Werlein <jwerlein2@student.cvtc.edu>
2025-08-27 20:51:03 -0500

    Create prices.js

---
+++ js/prices.js
@@ -0,0 +1,27 @@
+window.pricesByPath = {
+  // Handguns
+  "images/guns/handguns/9mmGlock.jpg": 529.99,
+  "images/guns/handguns/9mmHandgun.jpg": 399.00,          // generic 9mm handgun
+  "images/guns/handguns/HiPoint9mm.jpg": 189.99,
+  "images/guns/handguns/WalterPPK.jpg": 699.00,           // Walther PPK
+  "images/guns/handguns/beretta-handgun-001.jpg": 749.00,
+  "images/guns/handguns/beretta-handgun-002.jpg": 769.00,
+  "images/guns/handguns/smith-wesson-handgun-001.jpg": 649.00,
+  "images/guns/handguns/taurus-handgun-001.jpg": 349.00,
+
+  // Revolvers
+  "images/guns/revolvers/38Special.jpg": 499.00,
+  "images/guns/revolvers/38spl-blazer-001.jpg": 519.00,
+  "images/guns/revolvers/Smith&Wesson9mmRevolver.jpg": 799.00,
+  "images/guns/revolvers/colt-revolver-001.jpg": 1199.00,
+
+  // Rifles
+  "images/guns/rifles/AR-15.jpg": 799.00,
+  "images/guns/rifles/BarretAR15.jpg": 1999.00,           // Barrett
+  "images/guns/rifles/SpringfieldRifle.jpg": 1099.00,
+  "images/guns/rifles/Winchester1895.jpg": 1599.00,
+
+  // Shotguns
+  "images/guns/shotguns/Browning12Ga.jpg": 1299.00,
+  "images/guns/shotguns/Remington12Ga.jpg": 599.00,
+};

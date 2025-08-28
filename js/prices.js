3f7e7d88f7a2072a219367cf69168d310da08fa5
Joshua Werlein <jwerlein2@student.cvtc.edu>
2025-08-28 15:40:23 -0500

    Update prices.js

--- js/prices.js
+++ js/prices.js
@@ -1,25 +1,25 @@
 window.pricesByPath = {
   // Handguns
-  "images/guns/handguns/9mmGlock.jpg": 529.99,
-  "images/guns/handguns/HiPoint9mm.jpg": 189.99,
-  "images/guns/handguns/WaltherPPK.jpg": 699.00, 
-  "images/guns/handguns/beretta-handgun-001.jpg": 749.00,
-  "images/guns/handguns/springfield-handgun.jpg": 769.00,
-  "images/guns/handguns/smith-wesson-handgun-001.jpg": 649.00,
-  "images/guns/handguns/taurus-handgun-001.jpg": 349.00,
+  "images/guns/handguns/9mmGlock-001.jpg": 529.99,
+  "images/guns/handguns/HiPoint9mm-001.jpg": 189.99,
+  "images/guns/handguns/WaltherPPK-001.jpg": 699.00, 
+  "images/guns/handguns/beretta-9mm-001.jpg": 749.00,
+  "images/guns/handguns/springfield-handgun-001.jpg": 769.00,
+  "images/guns/handguns/smith-wesson-9mm-001.jpg": 649.00,
+  "images/guns/handguns/taurus-10mm-001.jpg": 349.00,
 
   // Revolvers
-  "images/guns/revolvers/38Special.jpg": 499.00,
-  "images/guns/revolvers/Smith&Wesson9mmRevolver.jpg": 799.00,
+  "images/guns/revolvers/38Special-001.jpg": 499.00,
+  "images/guns/revolvers/smith-wesson-revolver-9mm-001.jpg": 799.00,
   "images/guns/revolvers/colt-revolver-001.jpg": 1199.00,
 
   // Rifles
-  "images/guns/rifles/AR-15.jpg": 799.00,
-  "images/guns/rifles/BarretAR15.jpg": 1999.00,           // Barrett
-  "images/guns/rifles/SpringfieldRifle.jpg": 1099.00,
-  "images/guns/rifles/Winchester1895.jpg": 1599.00,
+  "images/guns/rifles/smith-wesson-rifle-001.jpg": 799.00,
+  "images/guns/rifles/Barret-AR15-001.jpg": 1999.00,  
+  "images/guns/rifles/SpringfieldRifle-001.jpg": 1099.00,
+  "images/guns/rifles/Winchester1895-001.jpg": 1599.00,
 
   // Shotguns
-  "images/guns/shotguns/Browning12Ga.jpg": 1299.00,
-  "images/guns/shotguns/Remington12Ga.jpg": 599.00,
+  "images/guns/shotguns/Browning12Ga-001.jpg": 1299.00,
+  "images/guns/shotguns/Remington12Ga-001.jpg": 599.00,
 };
